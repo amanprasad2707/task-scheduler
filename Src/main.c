87884edc613733387fe0e8e06bfcf3ce1bdb332f
Aman Prasad <aprasad2707@gmail.com>
2026-01-03 20:48:02 +0530

    Implement SysTick timer initialization and configuration

--- Src/main.c
+++ Src/main.c
@@ -16,14 +16,21 @@
 #define SCHED_STACK_START       ((SRAM_END) - (4 * SRAM_SIZE))
 
 
+#define TICK_HZ                 1000U
+#define HSI_CLK_FREQ            16000000U   // 16 MHz
+#define SYSTICK_TIM_CLK         HSI_CLK_FREQ
+
+
 void task1_handler(void);
 void task2_handler(void);
 void task3_handler(void);
 void task4_handler(void);
 
+void init_systick_timer(uint32_t tick_hz);
+
 int main(void){
     
-    
+    init_systick_timer(TICK_HZ);
     /* Loop forever */
 	for(;;);
 }
@@ -56,5 +63,27 @@ void task4_handler(void){
         printf("this is task 4\n");
         
     }
+    
+}
+
+
+void init_systick_timer(uint32_t tick_hz){
+    uint32_t *pSRVR = (uint32_t *) 0xE000E014; // systick relaod value register address
+    uint32_t *pSCSR = (uint32_t *) 0xE000E010; // systick control and status register address
+    uint32_t count_value = (SYSTICK_TIM_CLK / tick_hz) - 1;
+
+    // clear the value of SRVR
+    *pSRVR &= ~(0x00FFFFFF);
+
+    // load the value in the SRVR
+    *pSRVR |= count_value;
+
+    // do some systick config
+    *pSCSR |= (1 << 1); // enables systick exception request
+    *pSCSR |= (1 << 2); // sets the processors clock source (not external)
+    *pSCSR |= (1 << 0); // enables the counter
+}
+
+void SysTick_Handler(void){
     
 }
