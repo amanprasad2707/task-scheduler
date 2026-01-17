e881c23a8f01c6e1b3561900768a8b6059e66d5e
Aman Prasad <aprasad2707@gmail.com>
2026-01-17 13:14:03 +0530

    add task functions for LED control and initialize tasks in main

--- Src/main.c
+++ Src/main.c
@@ -1,5 +1,49 @@
 #include "main.h"
+#include "led.h"
+#include "scheduler.h"
+#include "tasks.h"
 
+
+void idle_task(void *arg){
+    while(1);
+}
+
+
+void green_task(void *arg){
+    while(1){
+        led_on(LED_GREEN);
+        task_delay(1000);
+        led_off(LED_GREEN);
+        task_delay(1000);
+    }
+}
+
+void red_task(void *arg){
+    while(1){
+        led_on(LED_RED);
+        task_delay(500);
+        led_off(LED_RED);
+        task_delay(500);
+    }
+}
+
+void blue_task(void *arg){
+    while(1){
+        led_on(LED_BLUE);
+        task_delay(250);
+        led_off(LED_BLUE);
+        task_delay(250);
+    }
+}
+
+void orange_task(void *arg){
+    while (1){
+        led_on(LED_ORANGE);
+        task_delay(125);
+        led_off(LED_ORANGE);
+        task_delay(125);
+    }
+}
 
 int main(void){
 
@@ -9,6 +53,12 @@ int main(void){
 
 
     led_init_all();
+
+    task_create(idle_task, NULL, 256, TASK_PRIORITY_IDLE);
+    task_create(green_task, NULL, 512, TASK_PRIORITY_MEDIUM);
+    task_create(blue_task, NULL, 512, TASK_PRIORITY_MEDIUM);
+    task_create(red_task, NULL, 512, TASK_PRIORITY_MEDIUM);
+    task_create(orange_task, NULL, 512, TASK_PRIORITY_MEDIUM);
 
     init_systick_timer(TICK_HZ);
 
